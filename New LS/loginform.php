15c7df5f48232c4841e3ba259f168f9a7b834a98
RafaelPalmaIII <rafaeliiipalma@gmail.com>
2024-12-17 08:08:26 +0800

    Hehewow

--- New LS/loginform.php
+++ New LS/loginform.php
@@ -1,18 +1,3 @@
-<?php
-// Connect to the database
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "palmals_main";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-?>
 <!DOCTYPE html>
 <html>
 <head>
